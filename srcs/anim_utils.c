990d8e6defa5e0f02e52f18627197029c626e357
Diogo Alexandre Costa Da Silva Gouveia <dicosta-@c2r3s3.42lisboa.com>
2025-11-21 22:31:36 +0000

    initiated int array on darken inside anim_utils.c

--- srcs/anim_utils.c
+++ srcs/anim_utils.c
@@ -21,7 +21,7 @@ void	draw_dim_img(t_data *src, t_data *dst, int x, int y, float factor)
 	while (sy < 64)
 	{
 		sx = 0;
-		if(sy + y > 1080)
+		if (sy + y > 1080)
 			return ;
 		while (sx < 64)
 		{
@@ -35,25 +35,25 @@ void	draw_dim_img(t_data *src, t_data *dst, int x, int y, float factor)
 
 void	darken(t_data src, float st_factor, float max_factor)
 {
-	int	var2;
-	int	var;
-	t_data temp;
-	while(st_factor >= max_factor)
+	t_data	temp;
+	int		arr[2];
+
+	while (st_factor >= max_factor)
 	{
-		var2 = 0;
+		arr[1] = 0;
 		temp.img = mlx_new_image(game()->mlx, 1920, 1080);
 		temp.addr = mlx_get_data_addr(temp.img,
 				&temp.bits_per_pixel, &temp.line_length,
 				&temp.endian);
-		while (var2 < 18)
+		while (arr[1] < 18)
 		{
-			var = 0;
-			while (var < 30)
+			arr[0] = 0;
+			while (arr[0] < 30)
 			{
-				draw_dim_img(&src, &temp, (var * 64), (var2 * 64), st_factor);
-				var++;
+				draw_dim_img(&src, &temp, (arr[0] * 64), (arr[1] * 64), st_factor);
+				arr[0]++;
 			}
-			var2++;
+			arr[1]++;
 		}
 		mlx_put_image_to_window(game()->mlx, game()->win, temp.img, 0, 0);
 		mlx_destroy_image(game()->mlx, temp.img);
@@ -63,10 +63,11 @@ void	darken(t_data src, float st_factor, float max_factor)
 
 void	lighten(t_data src, float st_factor)
 {
-	int	var2;
-	int	var;
-	t_data temp;
-	while(st_factor <= 1.0)
+	t_data	temp;
+	int		var2;
+	int		var;
+
+	while (st_factor <= 1.0)
 	{
 		var2 = 0;
 		temp.img = mlx_new_image(game()->mlx, 1920, 1080);
